2d9f23a2fd26d8ac2cc977275fec9e0f703b7bda
Maxime Cohen <maxime.cohen-pro@outlook.fr>
2017-12-12 11:11:54 +0100

    update db hids

--- server_agent/hids_db.sql
+++ server_agent/hids_db.sql
@@ -21,15 +21,15 @@ CREATE TABLE Site (
 	CONSTRAINT PK_site PRIMARY KEY (site_id)
 );
 
-CREATE TABLE User (
-  user_id INTEGER NOT NULL,
-  user_role_id INTEGER NOT NULL,
-  user_date DATE NOT NULL,
-  user_mail VARCHAR(70) NOT NULL,
-  user_login VARCHAR(30) NOT NULL,
-  user_password VARCHAR(255) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id),
-	CONSTRAINT FK_user_role FOREIGN KEY (user_role_id) REFERENCES Role(role_id)
+CREATE TABLE Person (
+  pers_id INTEGER NOT NULL,
+  pers_role_id INTEGER NOT NULL,
+  pers_date DATE NOT NULL,
+  pers_mail VARCHAR(70) NOT NULL,
+  pers_login VARCHAR(30) NOT NULL,
+  pers_password VARCHAR(255) NOT NULL,
+	CONSTRAINT PK_pers PRIMARY KEY (pers_id),
+	CONSTRAINT FK_pers_role FOREIGN KEY (pers_role_id) REFERENCES Role(role_id)
 );
 
 CREATE TABLE Hash (
@@ -38,17 +38,17 @@ CREATE TABLE Hash (
   hash_site_id INTEGER NOT NULL,
   hash_status_id INTEGER NOT NULL,
   hash_file_name VARCHAR(70) NOT NULL,
-  hash_file_path VARCHAR(255) NOT NULL
+  hash_file_path VARCHAR(255) NOT NULL,
 	CONSTRAINT PK_hash PRIMARY KEY (hash_id),
 	CONSTRAINT FK_hash_site FOREIGN KEY (hash_site_id) REFERENCES Site(site_id),
 	CONSTRAINT FK_hash_status FOREIGN KEY (hash_status_id) REFERENCES Status(status_id)
 );
 
-CREATE TABLE UserSite (
-  us_id INTEGER NOT NULL,
-  us_user_id INTEGER NOT NULL,
-  us_site_id INTEGER NOT NULL
-	CONSTRAINT PK_us PRIMARY KEY (us_id),
-	CONSTRAINT FK_us_user FOREIGN KEY (us_user_id) REFERENCES User(user_id),
-	CONSTRAINT FK_us_site FOREIGN KEY (us_site_id) REFERENCES Site(site_id)
+CREATE TABLE PersonSite (
+  ps_id INTEGER NOT NULL,
+  ps_pers_id INTEGER NOT NULL,
+  ps_site_id INTEGER NOT NULL,
+	CONSTRAINT PK_ps PRIMARY KEY (ps_id),
+	CONSTRAINT FK_ps_pers FOREIGN KEY (ps_pers_id) REFERENCES Person(pers_id),
+	CONSTRAINT FK_ps_site FOREIGN KEY (ps_site_id) REFERENCES Site(site_id)
 );
